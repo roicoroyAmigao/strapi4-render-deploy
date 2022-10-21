12d39bf8c7b804b2289d17c8a74a091773c8a501
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-10-21 21:37:57 +0100

    removed

--- src/plugins/strapi-plugin-fcm/admin/src/pluginId.js
+++ src/plugins/strapi-plugin-fcm/admin/src/pluginId.js
@@ -1,19 +1,19 @@
 const pluginPkg = require('../../settings.json');
 
-const firebase = require("firebase");
+// const firebase = require("firebase");
 
-const config = {
-    firebase: {
-        apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
-        authDomain: "amigao-fcm.firebaseapp.com",
-        projectId: "amigao-fcm",
-        storageBucket: "amigao-fcm.appspot.com",
-        messagingSenderId: "139473613556",
-        appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
-        measurementId: "G-DW9N8LQLDQ"
-    },
-};
-firebase.initializeApp(config);
+// const config = {
+//     firebase: {
+//         apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
+//         authDomain: "amigao-fcm.firebaseapp.com",
+//         projectId: "amigao-fcm",
+//         storageBucket: "amigao-fcm.appspot.com",
+//         messagingSenderId: "139473613556",
+//         appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
+//         measurementId: "G-DW9N8LQLDQ"
+//     },
+// };
+// firebase.initializeApp(config);
 
 const pluginId = pluginPkg.name;// .replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
 
