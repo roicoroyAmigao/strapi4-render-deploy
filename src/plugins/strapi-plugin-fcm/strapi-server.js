ae73dc66517a7d99778ac6e0cc9b96e6eaf4e9e5
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-10-21 21:00:48 +0100

    yarn fcm

--- src/plugins/strapi-plugin-fcm/strapi-server.js
+++ src/plugins/strapi-plugin-fcm/strapi-server.js
@@ -1,18 +1,18 @@
 'use strict';
 
-// const firebase = require("firebase");
+const firebase = require("firebase");
 
-// const config = {
-//     firebase: {
-//         apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
-//         authDomain: "amigao-fcm.firebaseapp.com",
-//         projectId: "amigao-fcm",
-//         storageBucket: "amigao-fcm.appspot.com",
-//         messagingSenderId: "139473613556",
-//         appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
-//         measurementId: "G-DW9N8LQLDQ"
-//     },
-// };
-// firebase.initializeApp(config);
+const config = {
+    firebase: {
+        apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
+        authDomain: "amigao-fcm.firebaseapp.com",
+        projectId: "amigao-fcm",
+        storageBucket: "amigao-fcm.appspot.com",
+        messagingSenderId: "139473613556",
+        appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
+        measurementId: "G-DW9N8LQLDQ"
+    },
+};
+firebase.initializeApp(config);
 
 module.exports = require('./server');
