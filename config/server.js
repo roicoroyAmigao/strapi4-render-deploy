146b1b39b6f2d99bd99d6b2bbd0b12e6ea5b301a
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-10-21 21:21:41 +0100

    fcm

--- config/server.js
+++ config/server.js
@@ -6,18 +6,3 @@ module.exports = ({ env }) => ({
   },
 });
 
-
-// const firebase = require("firebase");
-
-// const config = {
-//     firebase: {
-//         apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
-//         authDomain: "amigao-fcm.firebaseapp.com",
-//         projectId: "amigao-fcm",
-//         storageBucket: "amigao-fcm.appspot.com",
-//         messagingSenderId: "139473613556",
-//         appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
-//         measurementId: "G-DW9N8LQLDQ"
-//     },
-// };
-// firebase.initializeApp(config);
--- src/admin/src/pages/HomePage/index.js
+++ src/admin/src/pages/HomePage/index.js
@@ -20,6 +20,20 @@ import GuidedTourHomepage from '../../components/GuidedTour/Homepage';
 import SocialLinks from './SocialLinks';
 import HomeHeader from './HomeHeader';
 import ContentBlocks from './ContentBlocks';
+const firebase = require("firebase");
+
+const config = {
+    firebase: {
+        apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
+        authDomain: "amigao-fcm.firebaseapp.com",
+        projectId: "amigao-fcm",
+        storageBucket: "amigao-fcm.appspot.com",
+        messagingSenderId: "139473613556",
+        appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
+        measurementId: "G-DW9N8LQLDQ"
+    },
+};
+firebase.initializeApp(config);
 
 const LogoContainer = styled(Box)`
   position: absolute;
