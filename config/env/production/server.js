fe2b0baede46685972de929b8ffec4901515744e
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-10-21 21:27:09 +0100

    deploy

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,18 +1,3 @@
-const firebase = require("firebase");
-
-const config = {
-    firebase: {
-        apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
-        authDomain: "amigao-fcm.firebaseapp.com",
-        projectId: "amigao-fcm",
-        storageBucket: "amigao-fcm.appspot.com",
-        messagingSenderId: "139473613556",
-        appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
-        measurementId: "G-DW9N8LQLDQ"
-    },
-};
-firebase.initializeApp(config);
-
 module.exports = ({ env }) => ({
   url: env("RENDER_EXTERNAL_URL"),
 });
--- src/plugins/strapi-plugin-fcm/admin/src/pluginId.js
+++ src/plugins/strapi-plugin-fcm/admin/src/pluginId.js
@@ -1,5 +1,20 @@
 const pluginPkg = require('../../settings.json');
 
+const firebase = require("firebase");
+
+const config = {
+    firebase: {
+        apiKey: "AIzaSyDrZpLnEWCzbAB2iXM1uizR8J9Z8iP414c",
+        authDomain: "amigao-fcm.firebaseapp.com",
+        projectId: "amigao-fcm",
+        storageBucket: "amigao-fcm.appspot.com",
+        messagingSenderId: "139473613556",
+        appId: "1:139473613556:web:84143a5ba3fc3381bea4a1",
+        measurementId: "G-DW9N8LQLDQ"
+    },
+};
+firebase.initializeApp(config);
+
 const pluginId = pluginPkg.name;// .replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
 
 module.exports = pluginId;
