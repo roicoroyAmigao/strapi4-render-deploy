92fd1a0d95077e0f1cd24757a35032e4a70357df
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-10-22 08:41:51 +0100

    fcm custom plugin

--- config/database.js
+++ config/database.js
@@ -1,16 +1,3 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
