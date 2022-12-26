df754f7402736f80224fbcee931589507ecfa77f
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-12-26 09:39:33 +0000

    medusa added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const axios = require("axios");
 
 module.exports = {
   /**
@@ -18,5 +18,33 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   bootstrap({ strapi }) {
+    let seedData = axios.post(`http://localhost:9000/hooks/seed`);
+
+    seedData.then(async (res) => {
+
+      // console.log(res.data);
+
+      const products = await res?.data.products;
+      const regions = await res?.data.regions;
+      const shippingOptions = await res.data.shippingOptions;
+      const paymentProviders = await res.data.paymentProviders;
+      const fulfillmentProviders = await res?.data.fulfillmentProviders;
+      const shippingProfiles = await res?.data.shippingProfiles;
+
+      await strapi.services["api::fulfillment-provider.fulfillment-provider"].bootstrap(fulfillmentProviders);
+      await strapi.services["api::payment-provider.payment-provider"].bootstrap(paymentProviders);
+      await strapi.services["api::region.region"].bootstrap(regions);
+      await strapi.services["api::shipping-option.shipping-option"].bootstrap(shippingOptions);
+      await strapi.services["api::shipping-profile.shipping-profile"].bootstrap(shippingProfiles);
+      await strapi.services["api::product.product"].bootstrap(products);
+
+      // const user = await strapi.services['plugin::users-permissions.user'];
+      // // user.count;
+
+      // user.exists().then((res) => {
+      //   console.log(res);
+      // });
+
+    });
   },
 };
